18d056e2a3864807509efbd85dc9d8cb922ecdc7
Brandon Humphry <brandonhumphry@gmail.com>
2020-05-21 22:27:26 -0700

    adding updates from server.js

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -4,9 +4,9 @@ var $saveNoteBtn = $(".save-note");
 var $newNoteBtn = $(".new-note");
 var $noteList = $(".list-container .list-group");
 
-
 // activeNote is used to keep track of the note in the textarea
 var activeNote = {};
+
 
 // A function for getting all notes from the db
 var getNotes = function() {
